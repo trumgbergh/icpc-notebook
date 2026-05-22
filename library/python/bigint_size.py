aaa4ffb4b4c3b9a4a3bb8e8ab332c09aa48be2db
trumgbergh <68943610+trumgbergh@users.noreply.github.com>
2026-05-22 17:22:15 +1000

    add python section

---
+++ library/python/bigint_size.py
@@ -0,0 +1,5 @@
+import sys
+# Increase the limit to 10,000 digits
+sys.set_int_max_str_digits(10000)
+# Disable the limit entirely (set to 0)
+sys.set_int_max_str_digits(0)
---
+++ library/python/input.py
@@ -0,0 +1,6 @@
+N = input()                             # returns a string
+N = int(input())                        # returns a single integer
+# if the input is given as some spaced integers
+A = input().split()                     # returns an array of strings
+A = [int(i) for i in input().split()]   # returns an array of integers
+C,D = [int(i) for i in input().split()] # returns two integer variables C & D 
