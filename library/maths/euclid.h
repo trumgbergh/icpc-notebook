7517765985e45c7d8a9460cac908ae27da3bd761
trumgbergh <68943610+trumgbergh@users.noreply.github.com>
2026-08-28 22:59:57 +1000

    drop i64 alias for int64_t

--- library/maths/euclid.h
+++ library/maths/euclid.h
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
-typedef int64_t i64;
-i64 euclid(i64 a, i64 b, i64 &x, i64 &y) {
+int64_t euclid(int64_t a, int64_t b, int64_t &x, int64_t &y) {
     if (!b) return x = 1, y = 0, a;
-    i64 d = euclid(b, a % b, y, x);
+    int64_t d = euclid(b, a % b, y, x);
     return y -= a / b * x, d;
 }
--- library/maths/modnum.hpp
+++ library/maths/modnum.hpp
@@ -1,19 +1,19 @@
 #include "euclid.h"
 
-const i64 mod = 17;  // change to something else
+const int64_t mod = 17;  // change to something else
 struct Mod {
-    i64 x;
-    Mod(i64 xx) : x(xx) {}
+    int64_t x;
+    Mod(int64_t xx) : x(xx) {}
     Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
     Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
     Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
     Mod operator/(Mod b) { return *this * invert(b); }
     Mod invert(Mod a) {
-        i64 x, y, g = euclid(a.x, mod, x, y);
+        int64_t x, y, g = euclid(a.x, mod, x, y);
         assert(g == 1);
         return Mod((x + mod) % mod);
     }
-    Mod operator^(i64 e) {
+    Mod operator^(int64_t e) {
         if (!e) return Mod(1);
         Mod r = *this ^ (e / 2);
         r = r * r;
